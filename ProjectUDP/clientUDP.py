903c03cbcd6e4f6bdb10b68fbf4db875842e5be5
Renato Serrano <renatom.sandrade@gmail.com>
2023-08-06 17:34:02 -0300

    REFACTOR[refactor/client-server]: fixed stuff

--- ProjectUDP/clientUDP.py
+++ ProjectUDP/clientUDP.py
@@ -3,62 +3,53 @@ from PIL import Image
 import os
 import time
 
-serverName = "localhost" #Servidor local
-serverPort = 12002 #Porta do servidor
+def send_file(client_socket, filename, server_address, buffer_size):
+    client_socket.sendto(filename.encode(), server_address)  # Send filename
+    with open(filename, "rb") as f:
+        data_file = f.read()
+        num_chunks = len(data_file) // buffer_size + (len(data_file) % buffer_size != 0)
+        client_socket.sendto(num_chunks.to_bytes(4, byteorder='big'), server_address)
+        
+        for i in range(0, len(data_file), buffer_size):
+            client_socket.sendto(data_file[i:i+buffer_size], server_address)  # Send file data
+    return num_chunks
 
-buffer_size = 1024 #Tamanho do buffer indicado no projeto (1KB)
+def receive_modified_file(client_socket, original_filename, buffer_size, num_chunks):
+    print(f"Esperando {num_chunks} partes do servidor...")
+    if original_filename.endswith('.jpg'):
+        expected_filename = "udp_sent_back.jpg"
+        with open(expected_filename, 'wb') as file:
+            for _ in range(num_chunks):
+                data, _ = client_socket.recvfrom(buffer_size)
+                file.write(data)
 
-clientSocket = socket(AF_INET, SOCK_DGRAM) #Criação do socket, AF_INET indica uso do IPv4 e SOCK_DGRAM indica uso do UDP
+        print("Recebido")
+        with open(expected_filename, 'rb') as file:
+            im = Image.open(file)
+            im.show()
+    else:
+        expected_filename = "udp_sent_back.txt"
+        received_text = ''
+        for _ in range(num_chunks):
+            data, _ = client_socket.recvfrom(buffer_size)
+            received_text += data.decode()
 
-files = os.listdir() #Lista de arquivos no diretório  
-    
-#Função para enviar arquivos, lendo o arquivo em binário e enviando-o em partes de tamanho buffer_size. Primeiro envia o nome do arquivo e depois o conteúdo
+        with open(expected_filename, 'w') as file:
+            file.write(received_text)
+        print("Recebido pelo cliente")
+
+server_name = "localhost"
+server_port = 12002
+buffer_size = 1024
+
+client_socket = socket(AF_INET, SOCK_DGRAM)
+
+files = os.listdir()
 
 for file in files:
     if file.endswith('.txt') or file.endswith('.jpg'):
-        clientSocket.sendto(file.encode(), (serverName, serverPort))  # Send filename
-        with open(file, "rb") as f:  # Open file in binary mode
-            dataFile = f.read()
-            count=0
-            for i in range(0, len(dataFile), buffer_size):
-                count+=1
-            count_bytes = count.to_bytes(4, byteorder='big')
-            clientSocket.sendto(count_bytes, (serverName, serverPort))
-            count = 0
-            for i in range(0, len(dataFile), buffer_size):
-                clientSocket.sendto(dataFile[i:i+buffer_size], (serverName, serverPort))  # Send file data
-                count+=1
-            print(count)
+        num_chunks_sent = send_file(client_socket, file, (server_name, server_port), buffer_size)
+        receive_modified_file(client_socket, file, buffer_size, num_chunks_sent)
         break
- 
-# recebendo file modificada do servidor
 
-modified_filename, serverAddress = clientSocket.recvfrom(buffer_size)
-modified_filename = modified_filename.decode()
-
-print(modified_filename)
-
-if modified_filename == "udp_sent.jpg":  # Recebe a imagem
-        
-        time.sleep(2)        
-        with open("udp_sent_back.jpg", 'wb') as arquivo2:
-            for i in range(count):
-                data, serverAddress = clientSocket.recvfrom(1024)
-                if data:
-                    arquivo2.write(data)
-                else:
-                    break
-
-            print("Recebido")
-
-
-else: # Recebe a o arquivo .txt
-    for i in range(count):
-        
-        mc_part, serverAddress = clientSocket.recvfrom(buffer_size)
-        with open("udp_sent_back.txt", "w") as arquivo:
-            part = mc_part.decode()
-            arquivo.write(part)
-            print("Recebido pelo cliente")
-    
-clientSocket.close() #Fechamento do socket
+client_socket.close()
--- readme.md
+++ readme.md
@@ -8,8 +8,8 @@
 ### Testando o Projeto
 - Primeiro, mova algum dos arquivos (apenas um) de imagem ou texto para o diretório do projeto UDP, manualmente ou utilizando o comando ```mv TestFiles/udp_sending.txt ProjectUDP``` no terminal (para mover o arquivo de imagem, basta trocar .txt por .jpg)
 - Certifique-se de ter alguma versão do python instalada na sua máquina. Como exemplo, usaremos o python3.x.x
-- Entre no terminal no diretório principal utilizando ```cd ProjectUDP/``` e em seguida execute os seguintes comandos em terminais diferentes para melhor visualização:
-    - ```python3 serverUDP.py``` => Para enviar um arquivo, digite 'send' no terminal do servidor. Para encerrar a conexão, digite 'quit'.
+- Entre no terminal no diretório principal utilizando ```cd ProjectUDP/``` e em seguida execute os seguintes comandos (em ordem!) em terminais diferentes para melhor visualização:
+    - ```python3 serverUDP.py``` => Para enviar um arquivo/iniciar a conexão, digite 'send' no terminal do servidor. Para encerrar a conexão, digite 'quit'.
     - ```python3 clientUDP.py```
 - Caso tenha escolhido o arquivo texto, além da modificação de nome do arquivo, também sera mostrado o conteúdo do mesmo
 - A imagem possui um tamanho maior, para demonstrar que caso o arquivo seja maior que o buffer, seja mandado em partes para o servidor e em seguida retornado
